7bc685c0072c00086f587198e6a2e5c93fdf9ed9
joesmithpinzon07-prog <joesmithpinzon07@gmail.com>
2025-12-03 20:56:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Velamour Pro');
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
